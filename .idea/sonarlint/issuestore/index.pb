f1b4bf2bf2798e125b7018d1e8662cf46648a3ed
unknown <sbarrera.96@hotmail.com>
2022-06-02 18:18:25 -0500

    documentacion realizada, proyecto terminado

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,10 @@
+
+:
+
+js/data.js,3\0\300f07cc9a4e3bf926dae533dcd3e2b7a41e9ea8
+:
+
+js/game.js,a\1\a11dffb636ca569515545ae88e1b79af1707c694
+:
+
+index.html,f\6\f6013a00b362253c64368d6eebc50ea2131754e2
--- js/game.js
+++ js/game.js
@@ -1,5 +1,10 @@
 import { data } from "./data.js";
 
+/**
+ *  Esta funcion obtiene la pregunta al azar segun el nivel en el que se encuentre el usuario
+ * @param {number} buscar El nivel en el que se encuentre el usuario
+ * @returns {object} Retorna el objeto que contiene la pregunta aleatoria que se debe contestar
+ */
 function obtenerPregunta(buscar) {
     let preguntasCategoria = [];
     data.forEach(element => {
@@ -8,37 +13,41 @@ function obtenerPregunta(buscar) {
         }
     });
     const azar = Math.floor(Math.random() * preguntasCategoria.length);
-    return preguntasCategoria[azar]
+    return preguntasCategoria[azar];
 }
 
+/**
+ *  Esta funcion almacena el puntaje del jugador actual en el localStorage preguntando por su nombre 
+ *  si desea que se guarde su puntaje
+ * @param {number} puntosJugador Cantidad de puntos que tiene el jugador
+ */
 function guardarJugador(puntosJugador) {
-    let text;
     let person = prompt("Ingrese su nombre si desea guardar su puntaje:", "");
     if (person == null || person == "") {
-        text = "User cancelled the prompt.";
+        console.log("Usuario canceló el ingreso del nombre");
     } else {
         localStorage.setItem(person, puntosJugador);
     }
 
 }
 
+/**
+ *  Esta funcion le da la orden al boton ACEPTAR de verificar si el usuario contesta marcando la opcion 
+ *  correcta o si es falsa.
+ */
 function aceptar() {
-    const buscar = sessionStorage.getItem('level');
+    const buscar = sessionStorage.getItem("level");
     let checkboxes = document.querySelectorAll("input:checked")
     let respuestaSeleccionada = [];
     checkboxes.forEach((element) => {
         respuestaSeleccionada.push(parseInt(element.id));
     });
-    console.log("respuesta elegida", respuestaSeleccionada[0]);
-    console.log("Respuesta correcta", datos.correcta)
     if (datos.correcta == respuestaSeleccionada[0] && buscar < 5) {
-        console.log("Respuesta correcta!!!!!!!!");
-        sessionStorage.setItem('level', parseInt(buscar) + 1);
+        sessionStorage.setItem("level", parseInt(buscar) + 1);
         let puntaje = parseInt(sessionStorage.getItem("score"));
         document.getElementById("puntaje").innerText = "Puntos acumulados: " + String(puntaje + datos.premio);
         sessionStorage.setItem("score", parseInt(puntaje + datos.premio));
-        datos = obtenerPregunta(sessionStorage.getItem('level'));
-        console.log("respuesta en aceptar: ", datos);
+        datos = obtenerPregunta(sessionStorage.getItem("level"));
         document.getElementById("nivelActual").innerText = "Nivel " + String(parseInt(buscar) + 1);
         document.getElementById("premio").innerText = "Premio pregunta actual: " + datos.premio;
         document.getElementById("pregunta").innerText = datos.pregunta;
@@ -53,20 +62,22 @@ function aceptar() {
         let puntaje = parseInt(sessionStorage.getItem("score"));
         const puntosJugador = puntaje + datos.premio;
         sessionStorage.setItem("score", parseInt(puntaje + datos.premio));
-        alert("Ganaste, felicitaciones!");
+        alert("¡Ganaste, felicitaciones!");
         guardarJugador(puntosJugador);
         location.reload();
     }
 
     else {
-        console.log("RESPUESTA INCORRECTA");
-        alert("Respuesta equivocada,Tu puntuación es 0");
+        alert("Respuesta equivocada, tu puntuación es 0");
         guardarJugador(0);
         location.reload();
-
     }
 }
 
+/**
+ *  Esta funcion permite abandonar el juego con los puntos acumulados hasta ese momento.
+ *  También reinicia el juego. 
+ */
 function salir() {
     const puntajeJugador = sessionStorage.getItem("score");
     alert("Te restiraste,Tu puntuación es: " + puntajeJugador);
@@ -74,12 +85,17 @@ function salir() {
     location.reload();
 }
 
-function mostrarTabla(datos) {
-    let pregunta = datos.pregunta;
-    let resp1 = datos.respuesta1;
-    let resp2 = datos.respuesta2;
-    let resp3 = datos.respuesta3;
-    let resp4 = datos.respuesta4;
+/**
+ *  Esta funcion muestra todos los elementos de la pregunta usando la estructura DOM.
+ *  Tambien muestra el historial de jugadores
+ * @param {object} dataTabla Elementos de la pregunta que se desea mostrar
+ */
+function mostrarTabla(dataTabla) {
+    let pregunta = dataTabla.pregunta;
+    let resp1 = dataTabla.respuesta1;
+    let resp2 = dataTabla.respuesta2;
+    let resp3 = dataTabla.respuesta3;
+    let resp4 = dataTabla.respuesta4;
 
     const div1 = document.createElement("div");
     div1.classList.add("App");
@@ -89,7 +105,7 @@ function mostrarTabla(datos) {
     nivelActual.id = "nivelActual";
 
     let puntaje = document.createElement("div");
-    puntaje.innerText = "Puntos acumlados: 0"
+    puntaje.innerText = "Puntos acumlados: 0";
     puntaje.id = "puntaje";
 
     let premio = document.createElement("div");
@@ -105,7 +121,7 @@ function mostrarTabla(datos) {
 
     //Pregunta
     const headerTabla = document.createElement("thead");
-    const trHeader = document.createElement('tr');
+    const trHeader = document.createElement("tr");
 
     let fila1 = document.createElement("th");
     fila1.innerText = pregunta;
@@ -116,11 +132,11 @@ function mostrarTabla(datos) {
     tabla.appendChild(headerTabla);
 
     //Respuesta 1
-    const trRespuesta1 = document.createElement('tr');
+    const trRespuesta1 = document.createElement("tr");
     let tdRespuesta1 = document.createElement("td");
-    tdRespuesta1.classList.add('text-center');
+    tdRespuesta1.classList.add("text-center");
 
-    let input1 = document.createElement('input');
+    let input1 = document.createElement("input");
     input1.classList.add("form-check-input");
     input1.setAttribute("type", "checkbox");
     input1.id = "1";
@@ -136,11 +152,11 @@ function mostrarTabla(datos) {
     tblBody.appendChild(trRespuesta1);
 
     //Respuesta 2
-    const trRespuesta2 = document.createElement('tr');
+    const trRespuesta2 = document.createElement("tr");
     let tdRespuesta2 = document.createElement("td");
-    tdRespuesta1.classList.add('text-center');
+    tdRespuesta1.classList.add("text-center");
 
-    let input2 = document.createElement('input');
+    let input2 = document.createElement("input");
     input2.classList.add("form-check-input");
     input2.setAttribute("type", "checkbox");
     input2.id = "2";
@@ -156,11 +172,11 @@ function mostrarTabla(datos) {
     tblBody.appendChild(trRespuesta2);
 
     //Respuesta 3
-    const trRespuesta3 = document.createElement('tr');
+    const trRespuesta3 = document.createElement("tr");
     let tdRespuesta3 = document.createElement("td");
-    tdRespuesta3.classList.add('text-center');
+    tdRespuesta3.classList.add("text-center");
 
-    let input3 = document.createElement('input');
+    let input3 = document.createElement("input");
     input3.classList.add("form-check-input");
     input3.setAttribute("type", "checkbox");
     input3.id = "3";
@@ -176,11 +192,11 @@ function mostrarTabla(datos) {
     tblBody.appendChild(trRespuesta3);
 
     //Respuesta 4
-    const trRespuesta4 = document.createElement('tr');
+    const trRespuesta4 = document.createElement("tr");
     let tdRespuesta4 = document.createElement("td");
-    tdRespuesta1.classList.add('text-center');
+    tdRespuesta1.classList.add("text-center");
 
-    let input4 = document.createElement('input');
+    let input4 = document.createElement("input");
     input4.classList.add("form-check-input");
     input4.setAttribute("type", "checkbox");
     input4.id = "4";
@@ -196,23 +212,23 @@ function mostrarTabla(datos) {
     tblBody.appendChild(trRespuesta4);
 
     //Botones
-    const trBotonAceptar = document.createElement('tr');
+    const trBotonAceptar = document.createElement("tr");
     let tdBotonAceptar = document.createElement("td");
-    tdBotonAceptar.classList.add('text-center', 'fa-5x');
+    tdBotonAceptar.classList.add("text-center", "fa-5x");
 
-    let botonAceptar = document.createElement('button');
+    let botonAceptar = document.createElement("button");
     botonAceptar.classList.add("btn", "btn-success", "btn-block");
     botonAceptar.setAttribute("type", "button");
     botonAceptar.id = "aceptar";
-    botonAceptar.innerHTML = "ACEPTAR"
+    botonAceptar.innerHTML = "ACEPTAR";
     botonAceptar.addEventListener("click", aceptar);
 
-    let botonSalir = document.createElement('button');
-    botonSalir.classList.add("btn", "btn-success", "btn-block");
+    let botonSalir = document.createElement("button");
+    botonSalir.classList.add("btn", "btn-danger", "btn-block");
     botonSalir.setAttribute("type", "button");
     botonSalir.id = "salir";
-    botonSalir.innerHTML = "SALIR"
-    botonSalir.addEventListener("click", salir)
+    botonSalir.innerHTML = "SALIR";
+    botonSalir.addEventListener("click", salir);
 
     tdBotonAceptar.appendChild(botonAceptar);
     tdBotonAceptar.appendChild(botonSalir);
@@ -221,29 +237,29 @@ function mostrarTabla(datos) {
     tblBody.appendChild(trBotonAceptar);
 
     tabla.appendChild(tblBody);
-    // appends <table> into <body>
+
     div1.appendChild(nivelActual);
     div1.appendChild(puntaje);
     div1.appendChild(premio);
     div1.appendChild(tabla);
     body.appendChild(div1);
 
-    console.log(localStorage);
     let keys = Object.keys(localStorage);
+    const historial = document.createElement("h5");
+    historial.innerText = "Historial de jugadores";
+    div1.appendChild(historial);
     keys.forEach(element => {
         if (parseInt(localStorage.getItem(element)) || localStorage.getItem(element) == 0) {
-            console.log(localStorage.getItem(element));
-            let aux = element;
-            aux = document.createElement("div");
+            let aux = document.createElement("div");
             aux.innerText = element + ": " + localStorage.getItem(element);
             div1.appendChild(aux);
         }
     });
 }
 
-sessionStorage.setItem('level', 1);
-let nivel = sessionStorage.getItem('level');
-sessionStorage.setItem('score', 0)
+sessionStorage.setItem("level", 1);
+let nivel = sessionStorage.getItem("level");
+sessionStorage.setItem("score", 0);
 let datos = obtenerPregunta(nivel);
 mostrarTabla(datos);
-console.log(datos);
+
